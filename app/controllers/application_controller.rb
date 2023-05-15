ceaf42c01d782f708d5328533c0bee8fb0af244a
fukumoto <>
2023-05-15 18:14:25 +0900

    更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :basic
+    def basic
+        authenticate_or_request_with_http_basic do |name, password|
+          name == "test0515" && password == "0515"
+        end
+    end
 end
