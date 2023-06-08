78234ebb77421c6f57b0afa95d4a3cc8f86a20b1
fukumoto <>
2023-06-08 17:56:56 +0900

    schedule.rbの設定

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,7 @@
 env :PATH, ENV['PATH'] # 絶対パスから相対パス指定
 set :output, 'log/cron.log' # ログの出力先ファイルを設定
-set :environment, :development # 環境を設定
+set :environment, :production
 
-every 1.minute do # 1分毎に実行
-  # 「Book」モデルの、「self.create_book」メソッドを実行
-  runner 'Batch::DataCreate.data_create'
+every 1.day, at: ['6:00 pm'] do
+    runner 'Batch::DataCreate.data_create'
 end
