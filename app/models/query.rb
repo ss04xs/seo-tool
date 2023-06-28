38fb8ecbbd061fc7d2865dda03b359f77be3d8ed
fukumoto <>
2023-06-28 14:34:12 +0900

    コミット

--- app/models/query.rb
+++ app/models/query.rb
@@ -2,6 +2,6 @@ class Query < ApplicationRecord
   belongs_to :site
   has_many :ranks
 
-  validates :name, presence: true, uniqueness: true
+  validates :keyword, presence: true, uniqueness: true
   validates :url, presence: true
 end
