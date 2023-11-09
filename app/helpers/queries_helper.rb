b80b180e94b48bbb43966d9f1db8f2f2c4be6481
fukumoto <>
2023-11-09 16:51:28 +0900

    期間別ランク比較を追加

--- app/helpers/queries_helper.rb
+++ app/helpers/queries_helper.rb
@@ -6,11 +6,25 @@ module QueriesHelper
         return dates = (week_ago..today).to_a
     end
 
+    def get_one_week_ago
+        today = Date.today
+        # 過去一週間の日付を計算
+        week_ago = today - 6 # 6日前から開始するため、6を引く
+        return week_ago
+    end
+
     def get_one_month
         today = Date.today
         # 過去一週間の日付を計算
         month_ago = today - 31 # 6日前から開始するため、6を引く
         return dates = (month_ago..today).to_a
+    end
+
+    def get_one_month_ago
+        today = Date.today
+        # 過去一週間の日付を計算
+        month_ago = today - 31 # 6日前から開始するため、6を引く
+        return month_ago
     end
 
     def get_ranks_last(query)
@@ -23,6 +37,20 @@ module QueriesHelper
         else
             return ""
         end
+    end
+
+    def get_rank_comparison(query,comparison_day)
+        ranks = query.ranks
+        ranks_last = ranks.last
+        ranks_day_comparison = ranks.find_by(created_at: comparison_day.beginning_of_day..comparison_day.end_of_day)
+        if ranks_last && ranks_last.gsp_rank.present? && ranks_day_comparison && ranks_day_comparison.gsp_rank.present?
+            ranks_last.gsp_rank - ranks_day_comparison.gsp_rank
+        else
+            ""
+        end
+    end
+
+    def get_comparison_month(query)
     end
 
     def get_detection_url_last(query)
