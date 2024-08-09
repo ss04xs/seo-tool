1fa11e6535e738729890103fd42e79fc653a8006
fukumoto <>
2024-08-09 19:49:32 +0900

    更新

--- app/lib/batch/data_create.rb
+++ app/lib/batch/data_create.rb
@@ -25,18 +25,40 @@ class Batch::DataCreate
       options.add_argument('--disable-gpu')
       options.add_argument('--no-sandbox')
       options.add_argument('--disable-dev-shm-usage')
-      options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+      #options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
 
-      driver = Selenium::WebDriver.for :chrome, options: options
+      text = "#{queries.size}のデータを処理します\n"
+
+      user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+      ]
+
+      count =0
+      
 
       queries.each do |query|
+        count += 1
         keyword = query.keyword
         query_url = query.url
         @subject_array = []
 
+        puts "#{keyword}の検索を開始します"
 
-        # Google検索クエリの組み立て
-        sleep(60+rand(10))
+        user_agent = user_agents.sample
+        options.add_argument("user-agent=#{user_agent}")
+
+        driver = Selenium::WebDriver.for :chrome, options: options
+
+
         # Google検索クエリの組み立て
         # URLを開く
         url = "https://www.google.co.jp/search?q=#{keyword}&num=100"
@@ -53,6 +75,8 @@ class Batch::DataCreate
           url = ""
           #title = div_rc.search('h3[@class="zBAuLc l97dzf"]')[0].text # タイトルを抽出
           title = result.css('h3').text
+          text += "#{count}回目#{i}// == #{keyword} == #{title}です\n"
+          puts "#{count}回目#{i}// == #{keyword} == #{title}です\n"
           link = result.css('a').first
           get_url = link['href'] if link
           if get_url.present?
@@ -63,6 +87,7 @@ class Batch::DataCreate
             url = ""
           end
         end
+        puts "#{count}回目のキーワード処理を実行しました。"
         if @subject_array[0].present?
           gsp_rank = @subject_array[0][0]
           gsp_url = @subject_array[0][1]
@@ -73,6 +98,9 @@ class Batch::DataCreate
         query.ranks.create(gsp_rank: gsp_rank,detection_url:gsp_url)
         success += 1
         create_last_time = Time.now
+         # Google検索クエリの組み立て
+        driver.quit
+        sleep(60+rand(10))
       end
       # ブラウザを終了
       driver.quit
