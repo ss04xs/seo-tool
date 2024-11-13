6dcb5cc3f5101aaff427099aab31d0f291b9f911
fukumoto <>
2024-11-13 17:31:58 +0900

    queryモデルの制約調整

--- app/models/query.rb
+++ app/models/query.rb
@@ -2,6 +2,6 @@ class Query < ApplicationRecord
   belongs_to :site
   has_many :ranks
 
-  validates :keyword, presence: true, uniqueness: true
+  validates :keyword, presence: true, uniqueness: { scope: :site_id }
   validates :url, presence: true
 end
