3de9d813b94861168594b1a3c8ba593e584950a6
fukumoto <>
2024-11-15 14:29:11 +0900

    更新

--- app/models/query.rb
+++ app/models/query.rb
@@ -2,6 +2,6 @@ class Query < ApplicationRecord
   belongs_to :site
   has_many :ranks
 
-  validates :keyword, presence: true, uniqueness: { scope: :site_id }
+  validates :keyword, presence: true, uniqueness: { scope: :url }
   validates :url, presence: true
 end
